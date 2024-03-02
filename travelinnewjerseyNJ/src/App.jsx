8246200256658c9e3eef101f2130b35c8d861be6
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-01 23:46:42 -0500

    added pictures and gif file

--- travelinnewjerseyNJ/src/App.jsx
+++ travelinnewjerseyNJ/src/App.jsx
@@ -13,9 +13,9 @@ const App = () => {
 
       <Card imgSrc="public/rugarden.jpg" imgAlt = "rutgersgardens" title = "Rutgers Gardens" description= "Rutgers Gardens is a 180-acre botanic garden, consisting of designed gardens, plant collections, a student farm, farmers market, and natural habitats. As part of Rutgers University, Rutgers Gardens serves as a living laboratory for faculty and staff, while providing experiential and service learning opportunities for undergraduate and graduate students." btnText = "Learn more" btnLink = "https://rutgersgardens.rutgers.edu/" />
 
-      <Card imgSrc="" imgAlt = "card-image" title = "card-title" description= "This is card description." buttonText = "Learn more" btnLink = "#" />
-
-      <Card imgSrc="" imgAlt = "card-image" title = "card-title" description= "This is card description." buttonText = "Learn more" btnLink = "#" />
+      <Card imgSrc="public/rushi.jpg" imgAlt = "card-image" title = "SHI Stadium" description= "Recent SHI Stadium upgrades and improvements include five new LED ribbon boards, additional men's bathrooms, a widened concourse along the North and West sides, new Homeland Security camera systems and a new Distributed Antenna System (DAS) that will enhance cellular service. In 2017, Rutgers athletics replaced the FieldTurf, installing a brand new playing surface." btnText = "Learn more" btnLink = "https://scarletknights.com/facilities/shi-stadium/2" />
+      
+      <Card imgSrc="public/rugem.jpg" imgAlt = "rugem" title = "Fitness & Wellness Classes" description= "Group fitness/wellness classes are provided at the 4 campus recreation centers, free of charge to all users (students, faculty, staff, and recreation members)." btnText = "Learn more" btnLink = "https://recreation.rutgers.edu/fitness-wellness/fitness-wellness-classes" />
       
     </div>
     </>
